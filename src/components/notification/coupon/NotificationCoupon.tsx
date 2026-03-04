19859d328d52c0e0ae81ef85f3247703e8af2baa
김창수 <chang9811@interplug.co.kr>
2026-03-04 09:45:20 +0900

    style: 알림 쿠폰 영역 추가

--- src/components/notification/coupon/NotificationCoupon.tsx
+++ src/components/notification/coupon/NotificationCoupon.tsx
@@ -14,10 +14,31 @@ export default function NotificationCoupon() {
             <div className="coupon-item">
               <div className="coupon-sub-tit">시즌음료 구매시 할인</div>
               <div className="coupon-main-tit">아메리카노 10%할인쿠폰</div>
-              <button className="coupon-down">쿠폰 다운로드</button>
+              <div className="coupon-data-wrap">
+                <button className="coupon-down ">
+                  쿠폰 다운로드<i className="down-icon"></i>
+                </button>
+              </div>
             </div>
             <div className="coupon-date-wrap">
               <div className="coupon-date">25.11.09 23:59까지 사용</div>
+              <div className="coupon-btn">
+                <button className="coupon-use-btn">
+                  쿠폰상세 <i className="arr-icon"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="notification-coupon-item ">
+            <div className="coupon-item used">
+              <div className="coupon-sub-tit">시즌음료 구매시 할인</div>
+              <div className="coupon-main-tit">아메리카노 10%할인쿠폰</div>
+              <div className="coupon-data-wrap">
+                <div className="coupon-used ">사용완료</div>
+              </div>
+            </div>
+            <div className="coupon-date-wrap">
+              <div className="coupon-date">발급일로부터 7일내 사용</div>
               <div className="coupon-btn">
                 <button className="coupon-use-btn">
                   쿠폰상세 <i className="arr-icon"></i>
